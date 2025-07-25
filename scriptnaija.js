b78afe71459d5198404ff55e15745021b0d8db35
Daramola Oluseye <daramolaoluseye22@gmail.com>
2025-07-25 08:00:34 +0100

    added news and sign up pages

--- scriptnaija.js
+++ scriptnaija.js
@@ -14,19 +14,19 @@ recipeCloseBtn.addEventListener('click', () => {
 // get meal list that matches with the ingredients
 function getMealList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    fetch(`https://localrecipes-0ad7.restdb.io/rest/smart-life-naija=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
         let html = "";
-        if(data.meals){
-            data.meals.forEach(meal => {
+        if(data){
+            data.forEach(meal => {
                 html += `
-                <div class="meal-item" data-id = "${meal.idMeal}">
+                <div class="meal-item" data-id = "${_id}">
                     <div class="meal-img">
-                        <img src="${meal.strMealThumb}" alt="food" />
+                        <img src="${Images}" alt="food" />
                     </div>
                     <div class="meal-name">
-                        <h3>${meal.strMeal}</h3>
+                        <h3>${RecipeName}</h3>
                         <a href="#" class="recipe-btn">Get Recipe</a>
                     </div>
                 </div>
@@ -47,7 +47,7 @@ function getMealRecipe(e){
     e.preventDefault();
     if(e.target.classList.contains('recipe-btn')){
         let mealItem = e.target.parentElement.parentElement;
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        fetch(`https://localrecipes-0ad7.restdb.io/rest/smart-life-naija=${mealItem.dataset.id}`)
         .then(response => response.json())
         .then(data => mealRecipeModal(data.meals));
     }
@@ -58,21 +58,24 @@ function mealRecipeModal(meal){
     console.log(meal);
     meal = meal[0];
     let html = `
-        <h2 class="recipe-title">${meal.strMeal}</h2>
-        <p class="recipe-category">${meal.strCategory}</p>
+        <h2 class="recipe-title">${RecipeName}</h2>
+        <p class="recipe-category">${Category}</p>
         <div class="recipe-instruct">
             <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
+            <p>${Instructions}</p>
         </div>
         <div class="recipe-meal-img">
-            <img src="${meal.strMealThumb}" alt="" />
+            <img src="${Images}" alt="" />
         </div>
         <div class="recipe-link">
-            <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+            <a href="${Video}" target="_blank">Watch Video</a>
         </div>
     `;
     mealDetailsContent.innerHTML = html;
     mealDetailsContent.parentElement.classList.add('showRecipe');
 }
+
+
+
 
 
